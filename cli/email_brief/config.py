20e7d2d96249c4af424ddeb5dbee521ffe17c9bb
rasingh5 <rasingh@demandbase.com>
2026-04-09 21:36:57 +0530

    changed AUTH_SERVER_URL from localhost to the vercel host

--- cli/email_brief/config.py
+++ cli/email_brief/config.py
@@ -14,7 +14,7 @@ GROQ_API_KEY = os.getenv("GROQ_API_KEY", "")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "")
 GMAIL_CLIENT_ID = os.getenv("GMAIL_CLIENT_ID", "")
 GMAIL_CLIENT_SECRET = os.getenv("GMAIL_CLIENT_SECRET", "")
-AUTH_SERVER_URL = os.getenv("AUTH_SERVER_URL", "http://localhost:3001")
+AUTH_SERVER_URL = os.getenv("AUTH_SERVER_URL", "https://email-brief-eight.vercel.app")
 BRIEFING_TIME = os.getenv("BRIEFING_TIME", "10:00")
 EMAIL_COUNT = int(os.getenv("EMAIL_COUNT", "50"))
 
